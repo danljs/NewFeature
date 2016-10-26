c92b627c7cb5f0d67c55ebee5dcdf103c586aeef
danljs <daniel.liu1973@gmail.com>
2016-10-25 21:19:12 -0400

    peek and sorted

--- src/newconsumer/ConsumerTest.java
+++ src/newconsumer/ConsumerTest.java
@@ -1,5 +1,12 @@
 package newconsumer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -25,6 +32,40 @@ public class ConsumerTest {
 		
 		Function<String,String> ft = (name)->{return name + "adsf";};
 		System.out.println(ft.apply("adsffad"));
+		
+		List<String> l = new ArrayList<String>();
+		l.add("1");
+		l.add("2");
+		l.add("3");
+		l.forEach(System.out::println);
+		
+		Random rnd = new Random();
+		rnd.ints().limit(10).forEach(System.out::println);
+		
+		//rnd.ints().sorted().limit(10).forEach(System.out::println);
+
+		rnd.ints().limit(10).sorted().forEach(System.out::println);
+		
+		try {
+			System.out.println("==================peek==============");
+			Files.list(Paths.get("."))
+			     .map(Path::getFileName)
+			     .peek(System.out::println)
+			     .forEach(p -> printNames(p.toString() + "oy"));
+			
+			System.out.println("===============sorted=================");
+			Files.list(Paths.get("."))
+			     .map(Path::getFileName) // still a path
+			     .map(Path::toString) // convert to Strings
+			     .filter(name -> name.endsWith(".md"))
+			     .sorted() // sort them alphabetically
+			     .limit(5) // first 5
+			     .forEach(System.out::println);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	private static void printNames(String name) {
