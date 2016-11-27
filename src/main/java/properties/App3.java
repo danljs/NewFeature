979b32eefc1fa7cc66a260a0b081849686fa95a9
danljs <daniel.liu1973@gmail.com>
2016-11-26 21:41:05 -0500

    porperties

---
+++ src/main/java/properties/App3.java
@@ -0,0 +1,34 @@
+package properties;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App3 {
+	
+	public static void main( String[] args ){
+
+		Properties prop = new Properties();
+		InputStream input = null;
+	
+		try {
+	
+			String filename = "configuration.properties";
+			input = App3.class.getClassLoader().getResourceAsStream(filename);
+			if(input==null){
+		            System.out.println("Sorry, unable to find " + filename);
+			    return;
+			}
+	
+			//load a properties file from class path, inside static method
+			prop.load(input);
+	
+	            //get the property value and print it out
+	            System.out.println(prop.getProperty("email"));
+		        System.out.println(prop.getProperty("sms"));
+		        System.out.println(prop.getProperty("voice"));
+	
+		} catch (IOException ex) {
+			ex.printStackTrace();
+	    }
+	}
+}
