9aa3fefe186991c53764d47331087e81c3427eb8
Daniel Liu <daniel.liu1973@gmail.com>
2016-11-30 23:28:17 -0500

    list

---
+++ src/main/java/collection/ListTest.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+
+		List<String> l1 = new ArrayList<String>();
+		l1.add("l1");
+		l1.add("l2");
+		l1.add("l3");
+		
+		List<String> m1 = new ArrayList<String>();
+		m1.addAll(l1);
+	}
+
+}
